7abfa4d56d43642dccadba86345c4fed359e05f9
iJustWantTooSee <64579385+iJustWantTooSee@users.noreply.github.com>
2020-05-07 02:20:02 +0700

    second commit

--- app/src/main/java/com/example/ourfirstphotoeditor/MainActivity.kt
+++ app/src/main/java/com/example/ourfirstphotoeditor/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.ourfirstphotoeditor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        takePhotoButton.text="Take a photo"
+
+        takeFromGalleryButton.text="Сhoice a photo from the gallery"
+
+        infAboutTeamButton.text="О команде"
+
+
     }
+
+
 }
