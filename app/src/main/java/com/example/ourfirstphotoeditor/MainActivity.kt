a23559f1b1eca81d34b32b0b1d70601ae9256170
iJustWantTooSee <64579385+iJustWantTooSee@users.noreply.github.com>
2020-05-08 14:13:37 +0700

    Camera

--- app/src/main/java/com/example/ourfirstphotoeditor/MainActivity.kt
+++ app/src/main/java/com/example/ourfirstphotoeditor/MainActivity.kt
@@ -1,28 +1,36 @@
 package com.example.ourfirstphotoeditor
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAKE_PICTURE = 1
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
-
         infAboutTeamButton.text="О команде"
 
         infAboutTeamButton.setOnClickListener {
 
-            Toast.makeText(this@MainActivity, "Test", Toast.LENGTH_LONG).show()
+            Toast.makeText(this@MainActivity, "Наша команда: Виктор, Дарья, Кирилл", Toast.LENGTH_LONG).show()
         }
 
         cameraButton.setOnClickListener {
+
+            startActivityForResult(cameraIntent, TAKE_PICTURE)
+
+
             var intent = Intent(MainActivity@this, SecondActivity::class.java)
             startActivity(intent)
         }
