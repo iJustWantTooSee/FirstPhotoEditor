ba62529c592d8ea0e37f426cfd4b45a405781fb2
iJustWantTooSee <zabilparol1234sos@gmail.com>
2020-05-08 12:58:35 +0300

    ViktorDodelai

--- app/src/main/java/com/example/ourfirstphotoeditor/MainActivity.kt
+++ app/src/main/java/com/example/ourfirstphotoeditor/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.ourfirstphotoeditor
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Toast
@@ -13,7 +15,9 @@ class MainActivity : AppCompatActivity() {
     companion object {
         const val TAKE_PICTURE = 1
         val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        val REQUEST_TAKE_PHOTO = 1
     }
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,7 +32,14 @@ class MainActivity : AppCompatActivity() {
         }
 
         cameraButton.setOnClickListener {
-            
+
+         /*   storageDir = File(
+                Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_PICTURES
+                ),
+                getAlbumName()
+            )*/
+
             var intent = Intent(MainActivity@this, SecondActivity::class.java)
             startActivity(intent)
             startActivityForResult(cameraIntent, TAKE_PICTURE)
@@ -40,7 +51,26 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+   /* override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == Activity.RESULT_OK) {
+            imageView.setImageURI(photoURI)
+        }
+    }
 
 
+    fun onClick(view: View?) {
+        dispatchTakePictureIntent()
+    }
 
+    private fun handleSmallCameraPhoto(intent: Intent) {
+        val extras = intent.extras
+        mImageBitmap = extras!!["data"] as Bitmap?
+        mImageView.setImageBitmap(mImageBitmap)
+    }
+*/
 }
