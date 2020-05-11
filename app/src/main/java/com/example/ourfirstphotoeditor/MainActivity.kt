fa1138f34e9afe59b345b5a61fc8acfc29d0514c
iJustWantTooSee <64579385+iJustWantTooSee@users.noreply.github.com>
2020-05-12 02:07:34 +0700

    Camera

--- app/src/main/java/com/example/ourfirstphotoeditor/MainActivity.kt
+++ app/src/main/java/com/example/ourfirstphotoeditor/MainActivity.kt
@@ -2,29 +2,37 @@ package com.example.ourfirstphotoeditor
 
 import android.app.Activity
 import android.content.Intent
-import android.graphics.Bitmap
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.Manifest
+import android.content.ContentValues
+import android.os.Environment
 import android.provider.MediaStore
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import kotlinx.android.synthetic.main.activity_main.*
-
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
-        const val TAKE_PICTURE = 1
-        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        val REQUEST_TAKE_PHOTO = 1
+       // val REQUEST_IMAGE_CAPTURE = 1
     }
-
+    private val PERMISSION_CODE = 1000;
+    private val IMAGE_CAPTURE_CODE = 1001
+    var image_uri: Uri? = null
+    //private val photoURI: Uri? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
 
         d_cube.setOnClickListener {
 
@@ -32,47 +40,138 @@ class MainActivity : AppCompatActivity() {
         }
 
         cameraButton.setOnClickListener {
-
-         /*   storageDir = File(
-                Environment.getExternalStoragePublicDirectory(
-                    Environment.DIRECTORY_PICTURES
-                ),
-                getAlbumName()
-            )*/
-
-            var intent = Intent(MainActivity@this, SecondActivity::class.java)
-            startActivity(intent)
-            startActivityForResult(cameraIntent, TAKE_PICTURE)
+            checkCamera()
         }
 
         takeFromGalleryButton.setOnClickListener {
+            //РЕМОНТ. ОТКРОЕТСЯ ЗАВТРА!
+          /*  var intent = Intent(MainActivity@this, SecondActivity::class.java)
+            startActivity(intent)*/
+        }
+    }
+
+
+    fun checkCamera(){
+        //if system os is Marshmallow or Above, we need to request runtime permission
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if (checkSelfPermission(Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_DENIED ||
+                checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_DENIED){
+                //permission was not enabled
+                val permission = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                //show popup to request permission
+                requestPermissions(permission, PERMISSION_CODE)
+            }
+            else{
+                //permission already granted
+                openCamera()
+            }
+        }
+        else{
+            //system os is < marshmallow
+            openCamera()
+        }
+    }
+
+
+    private fun openCamera() {
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "New Picture")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera")
+        image_uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        //camera intent
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri)
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        //called when user presses ALLOW or DENY from Permission Request Popup
+        when(requestCode){
+            PERMISSION_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED){
+                    //permission from popup was granted
+                    openCamera()
+                }
+                else{
+                    //permission from popup was denied
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        //called when image was captured from camera intent
+        if (resultCode == Activity.RESULT_OK){
+            //set image captured to image view
+           // image_view.setImageURI(image_uri)
             var intent = Intent(MainActivity@this, SecondActivity::class.java)
+            intent.putExtra("imageUri", image_uri);
             startActivity(intent)
         }
     }
 
-   /* override fun onActivityResult(
+    /*lateinit var currentPhotoPath: String
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+    val REQUEST_TAKE_PHOTO = 1
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    // Error occurred while creating the File
+
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "com.example.ourfirstphotoeditor.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
+                }
+            }
+        }
+    }
+    override fun onActivityResult(
         requestCode: Int,
         resultCode: Int,
         data: Intent?
     ) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_TAKE_PHOTO && resultCode == Activity.RESULT_OK) {
-            imageView.setImageURI(photoURI)
+            var intent = Intent(MainActivity@this, SecondActivity::class.java)
+            intent.putExtra("imageUri", photoURI);
+            startActivity(intent)
+
         }
     }
 
-
-    fun onClick(view: View?) {
-        dispatchTakePictureIntent()
-    }
-
-<<<<<<< HEAD
-    private fun handleSmallCameraPhoto(intent: Intent) {
-        val extras = intent.extras
-        mImageBitmap = extras!!["data"] as Bitmap?
-        mImageView.setImageBitmap(mImageBitmap)
-    }
 */
 
 }
--- app/src/main/java/com/example/ourfirstphotoeditor/SecondActivity.kt
+++ app/src/main/java/com/example/ourfirstphotoeditor/SecondActivity.kt
@@ -1,8 +1,9 @@
 package com.example.ourfirstphotoeditor
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
@@ -10,7 +11,8 @@ class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
-
+        val uri: Uri = intent.getParcelableExtra("imageUri")
+        image_view.setImageURI(uri);
         backMainActivity.setOnClickListener {
             var intent = Intent(SecondActivity@this, MainActivity::class.java)
             startActivity(intent)
