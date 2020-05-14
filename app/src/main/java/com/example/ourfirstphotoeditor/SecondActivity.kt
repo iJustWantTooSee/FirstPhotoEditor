4976842641a5666c25ec826e54f305f6bc4146b7
iJustWantTooSee <64579385+iJustWantTooSee@users.noreply.github.com>
2020-05-14 13:51:12 +0700

    chit

--- app/src/main/java/com/example/ourfirstphotoeditor/SecondActivity.kt
+++ app/src/main/java/com/example/ourfirstphotoeditor/SecondActivity.kt
@@ -1,17 +1,10 @@
 package com.example.ourfirstphotoeditor
 
 import android.content.Intent
-import android.content.res.Resources
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.net.Uri
-import android.os.Build.ID
 import android.os.Bundle
-import android.os.Environment
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_second.*
-import java.io.File
 
 public class SecondActivity : AppCompatActivity() {
 
@@ -19,9 +12,7 @@ public class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
         val uri: Uri = intent.getParcelableExtra("imageUri")
-
         image_view.setImageURI(uri);
-        image_view.setImageBitmap(decodeSampledBitmapFromResource(uri,R.ID.))
         backMainActivity.setOnClickListener {
             var intent = Intent(SecondActivity@this, MainActivity::class.java)
             startActivity(intent)
@@ -29,56 +20,6 @@ public class SecondActivity : AppCompatActivity() {
 
     }
 
-    fun preparingPhoto(){
-        val options = BitmapFactory.Options().apply {
-            inJustDecodeBounds = true
-        }
-        BitmapFactory.decodeResource(resources, R.id.image_view, options)
-        val imageHeight: Int = options.outHeight
-        val imageWidth: Int = options.outWidth
-        val imageType: String = options.outMimeType
-    }
 
-
-    fun decodeSampledBitmapFromResource(
-        res: Resources,
-        resId: Int,
-        reqWidth: Int,
-        reqHeight: Int
-    ): Bitmap {
-        // First decode with inJustDecodeBounds=true to check dimensions
-        return BitmapFactory.Options().run {
-            inJustDecodeBounds = true
-            BitmapFactory.decodeResource(res, resId, this)
-
-            // Calculate inSampleSize
-            inSampleSize = calculateInSampleSize(this, reqWidth, reqHeight)
-
-            // Decode bitmap with inSampleSize set
-            inJustDecodeBounds = false
-
-            BitmapFactory.decodeResource(res, resId, this)
-        }
-    }
-
-    fun calculateInSampleSize(options: BitmapFactory.Options, reqWidth: Int, reqHeight: Int): Int {
-        // Raw height and width of image
-        val (height: Int, width: Int) = options.run { outHeight to outWidth }
-        var inSampleSize = 1
-
-        if (height > reqHeight || width > reqWidth) {
-
-            val halfHeight: Int = height / 2
-            val halfWidth: Int = width / 2
-
-            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
-            // height and width larger than the requested height and width.
-            while (halfHeight / inSampleSize >= reqHeight && halfWidth / inSampleSize >= reqWidth) {
-                inSampleSize *= 2
-            }
-        }
-
-        return inSampleSize
-    }
 
 }
