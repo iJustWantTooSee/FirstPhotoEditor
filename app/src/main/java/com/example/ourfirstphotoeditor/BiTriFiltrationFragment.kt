82eb997efe51bd429d03fb4588fa819ca0100b0a
darubazz <darubazz@users.noreply.github.com>
2020-05-18 14:18:38 +0700

    fragment maket

---
+++ app/src/main/java/com/example/ourfirstphotoeditor/BiTriFiltrationFragment.kt
@@ -0,0 +1,19 @@
+package com.example.ourfirstphotoeditor
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class BiTriFiltrationFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bi_tri_filtration, container, false)
+    }
+
+}
--- app/src/main/java/com/example/ourfirstphotoeditor/SecondActivity.kt
+++ app/src/main/java/com/example/ourfirstphotoeditor/SecondActivity.kt
@@ -4,9 +4,11 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_second.*
 
 public class SecondActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,9 +19,12 @@ public class SecondActivity : AppCompatActivity() {
             var intent = Intent(SecondActivity@this, MainActivity::class.java)
             startActivity(intent)
         }
-
     }
 
-
+    public fun replaceFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fr_place, fragment)
+        transaction.commit()
+    }
 
 }
