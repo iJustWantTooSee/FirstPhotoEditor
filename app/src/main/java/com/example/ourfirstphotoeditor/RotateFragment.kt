06d4a124096035de86012eeba328ce3f3abb952a
darubazz <darubazz@users.noreply.github.com>
2020-05-20 16:33:56 +0700

    nothing new

--- app/src/main/java/com/example/ourfirstphotoeditor/RotateFragment.kt
+++ app/src/main/java/com/example/ourfirstphotoeditor/RotateFragment.kt
@@ -1,10 +1,15 @@
 package com.example.ourfirstphotoeditor
 
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
+import android.widget.TextView
+
 
 class RotateFragment : Fragment() {
 
