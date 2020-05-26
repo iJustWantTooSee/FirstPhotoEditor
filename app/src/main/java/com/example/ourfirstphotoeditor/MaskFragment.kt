fb89df43031b28b044b2e9b9618cdb8edfcd838a
darubazz <darubazz@users.noreply.github.com>
2020-05-26 12:32:45 +0700

    Mask

--- app/src/main/java/com/example/ourfirstphotoeditor/MaskFragment.kt
+++ app/src/main/java/com/example/ourfirstphotoeditor/MaskFragment.kt
@@ -1,12 +1,18 @@
 package com.example.ourfirstphotoeditor
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_second.*
+import kotlinx.android.synthetic.main.fragment_mask.*
 
 class MaskFragment : Fragment() {
+    var Photo: Bitmap? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -14,4 +20,37 @@ class MaskFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_mask, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (Photo == null) {
+            Photo = ((activity as SecondActivity)!!.image_view.drawable as BitmapDrawable).bitmap
+        }
+        textViewAmount.text = ""
+        textViewRadius.text = ""
+        textViewThreshold.text = ""
+
+        seekAmount.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                TODO("Not yet implemented")
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+        seekRadius.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                TODO("Not yet implemented")
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+        seekThreshold.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                TODO("Not yet implemented")
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+    }
+
 }
