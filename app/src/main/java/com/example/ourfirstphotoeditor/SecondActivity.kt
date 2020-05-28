df694734ee2632f987386a563f56d2f51b8a958c
darubazz <darubazz@users.noreply.github.com>
2020-05-28 20:27:34 +0700

    nothing new

--- app/src/main/java/com/example/ourfirstphotoeditor/SecondActivity.kt
+++ app/src/main/java/com/example/ourfirstphotoeditor/SecondActivity.kt
@@ -165,10 +165,8 @@ public class SecondActivity : AppCompatActivity(), changeInterface {
         try {
             MediaStore.Images.Media.insertImage(contentResolver, originBitmap, imageFileName, "Image of $title")
             Toast.makeText(this, getString(R.string.saved), Toast.LENGTH_SHORT).show()
-                //       toast("The picture has been saved")
         } catch (e: IOException) {
             Toast.makeText(this, getString(R.string.error), Toast.LENGTH_SHORT).show()
-     //       toast("I'm sorry I messed up somewhere and your photo didn't survive.")
         }
     }
 
